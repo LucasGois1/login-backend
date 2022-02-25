15c0abf11bda2871dd5a5913bc5ba4e336c5b5c5
Lucas Gois <lucas.gois@linnks.com.br>
2022-02-25 19:05:43 -0300

    feat: add server & env configs

---
+++ src/main/config/env.ts
@@ -0,0 +1,8 @@
+export default {
+  // mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/join-us-database',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://lucasgoisdev:LqlG1egm19Fc0xhW@joinus.l7okp.mongodb.net/JoinUS?retryWrites=true&w=majority',
+  port: process.env.PORT || 8000,
+  jwtSecret: process.env.JWT_SECRET || 'jks79812bjkb8¨55615vb¨%!',
+  emailUser: 'lucassampaiodegois1@gmail.com',
+  emailPassword: 'qyuienyvnbnpbkrd'
+}
