70266c77eb13c06ba1bcc3e66756057b7e17b1b9
Lucas Gois <lucas.gois@linnks.com.br>
2022-02-25 19:39:06 -0300

    feat: add new route /forgot-password

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { routeAdapter } from '../adapters/express/express-route-adapter'
+import { makeForgotPasswordController } from '../factorys/forgot-password/forgot-password-factory'
 import { makeLoginController } from '../factorys/login/login-factory'
 import { makeSignUpController } from '../factorys/signup/signup-factory'
 
 export default (router: Router): void => {
   router.post('/signup', routeAdapter(makeSignUpController()))
   router.post('/login', routeAdapter(makeLoginController()))
+  router.post('/forgot-password', routeAdapter(makeForgotPasswordController()))
 }
