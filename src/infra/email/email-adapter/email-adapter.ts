7a92a2591094f938a63ca4e39eda8b3f2aa5ffb8
Lucas Gois <lucas.gois@linnks.com.br>
2022-02-25 19:04:57 -0300

    feat: add email validator && adapter

---
+++ src/infra/email/email-adapter/email-adapter.ts
@@ -0,0 +1,30 @@
+import { nodemailer } from 'nodemailer'
+import EmailSender from '../../../data/protocols/email/email-sender'
+
+export default class EmailAdapter implements EmailSender {
+  constructor (
+    private readonly username: string,
+    private readonly password: string
+  ) {}
+
+  // async..await is not allowed in global scope, must use a wrapper
+  async send (subject: string, text: string, destinatary: string): Promise<boolean> {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: this.username,
+        pass: this.password // generated ethereal password
+      }
+    })
+
+    const response = await transporter.sendMail({
+      from: `"Lucas Góis 👻" <${this.username}>`,
+      to: destinatary,
+      subject,
+      text
+    })
+    return new Promise((resolve) => resolve(!!response))
+  }
+}
