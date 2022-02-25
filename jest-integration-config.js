483896cce7d9eaab2d3158766bb622ce61b7cce7
Lucas Gois <lucas.gois@linnks.com.br>
2022-02-25 19:01:33 -0300

    chore: jest config files

---
+++ jest-integration-config.js
@@ -0,0 +1,5 @@
+const config = require('./jest.config.ts')
+config.testMatch = ['**/*.test.ts']
+
+// eslint-disable-next-line eol-last
+module.exports = config
---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  clearMocks: true,
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/protocols/**',
+    '!<rootDir>/src/**/*protocols.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
